fb118d6098c54b6c67e6fd9de90865654dcd37ae
김다인 <dbslzhs1218@naver.com>
2024-04-03 00:43:12 +0900

    feat: Step5: 렌더와 커밋 단계(Render and Commit Phases)

--- build_own_your_react/didact.js
+++ build_own_your_react/didact.js
@@ -65,18 +65,38 @@ function createDom(fiber) {
   return dom;
 }
 
+// 전체 fiber트리를 DOM에 커밋
+function commitRoot() {
+  commitWork(wipRoot.child);
+  wipRoot = null;
+}
+
+// 모든 노드 재귀적으로 DOM에 추가
+function commitWork(fiber) {
+  if (!fiber) {
+    return;
+  }
+  const domParent = fiber.parent.dom;
+  domParent.appendChild(fiber.dom);
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
 // fiber 트리의 루트에 nextUnitOfWork 함수 설정
 function render(element, container) {
-  nextUnitOfWork = {
+  // fiber트리의 루트
+  wipRoot = {
     dom: container,
     props: {
-        children: [element],
-    }
-  }
+      children: [element],
+    },
+  };
+  nextUnitOfWork = wipRoot;
 }
 
 // 동시성 모드, 렌더링 도중 끼어들 수 있도록 구현
 let nextUnitOfWork = null; // 첫번째 작업 단위
+let wipRoot = null; // 작업중인 루트
 
 // deadline: 다시 브라우저에서 제어를 가져갈 때까지 얼마나 걸리는지 체크
 function workLoop(deadline) {
@@ -86,6 +106,13 @@ function workLoop(deadline) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
     shouldYield = deadline.timeRemaining() < 1;
   }
+
+  // 더이상 다음 작업이 없는 경우
+  if (!nextUnitOfWork && wipRoot) {
+    // 전체 fiber트리를 DOM에 커밋
+    commitRoot();
+  }
+
   requestIdleCallback(workLoop);
 }
 
@@ -94,65 +121,60 @@ requestIdleCallback(workLoop);
 
 // fiber 기능
 function performUnitOfWork(fiber) {
-    // DOM에 엘리먼트 추가
-    // 루트부터 작업을 시작할 떄 새로운 노드 생성 후 추가
-    if(!fiber.dom) {
-        fiber.dom = createDom(fiber);
+  // DOM에 엘리먼트 추가
+  // 루트부터 작업을 시작할 떄 새로운 노드 생성 후 추가
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  // 각 엘리먼트의 children에 대해 fiber 생성
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  // index가 elements배열 길이보다 작을 때 반복
+  while (index < elements.length) {
+    const element = elements[index];
+
+    // 새로운 fiber 생성
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    // 첫번째 자식이면
+    if (index === 0) {
+      // 자식으로 fiber 트리에 추가
+      fiber.child = newFiber;
+    } else {
+      // 형제자매로 fiber 트리에 추가
+      prevSibling.sibling = newFiber;
     }
 
-    
-    if (fiber.parent) {
-        fiber.parent.dom.appendchild(fiber.dom)
+    // 이전 형제를 생성 fiber로
+    prevSibling = newFiber;
+    index++;
+  }
+
+  // 다음 작업 단위 선택
+  // 자식 탐색
+  if (fiber.child) {
+    return fiber.children;
+  }
+
+  // 형제 자매로 이동
+  let nextFiber = fiber;
+
+  while (nextFiber) {
+    // 형제자매 탐색
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
     }
-
-    // 각 엘리먼트의 children에 대해 fiber 생성
-    const elements = fiber.props.children
-    let index = 0
-    let prevSibling = null
-
-    // index가 elements배열 길이보다 작을 때 반복
-    while (index < elements.length) {
-        const element = elements[index]
-
-        // 새로운 fiber 생성
-        const newFiber = {
-            type: element.type,
-            props: element.props,
-            parent: fiber,
-            dom: null,
-        }
-
-        // 첫번째 자식이면 
-        if (index === 0) {
-            // 자식으로 fiber 트리에 추가
-            fiber.child = newFiber
-        }else {
-            // 형제자매로 fiber 트리에 추가
-            prevSibling.sibling = newFiber
-        }
-    
-        // 이전 형제를 생성 fiber로
-        prevSibling = newFiber
-        index++
-    }
-
-    // 다음 작업 단위 선택
-    // 자식 탐색
-    if(fiber.child) {
-        return fiber.children
-    }
-
-    // 형제 자매로 이동
-    let nextFiber = fiber
-
-    while (nextFiber) {
-        // 형제자매 탐색
-        if(nextFiber.sibling) {
-            return nextFiber.sibling
-        }
-        // 부모의 형제자매로 이동
-        nextFiber = nextFiber.parent
-    }
+    // 부모의 형제자매로 이동
+    nextFiber = nextFiber.parent;
+  }
 }
 
 const Didact = {
