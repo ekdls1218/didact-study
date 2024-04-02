3e66e0ee77b622ec7beac4aeba490bea9db6776a
김다인 <dbslzhs1218@naver.com>
2024-04-03 00:32:54 +0900

    feat: Step4: Fibers

--- build_own_your_react/didact.js
+++ build_own_your_react/didact.js
@@ -34,78 +34,140 @@ function createElement(type, props, ...children) {
 }
 
 // children이 객체가 아닌 기본 타입의 값(string, number)일 때
-function createTextElement (text) {
-    return {
-        type: "TEXT_ELEMENT",
-        props: {
-            nodeValue: text,
-            children: [],
-        }
-    }
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
 }
 
-// ReactDOM.render 함수 구현, DOM 갱신과 삭제
-function render(element, container) {
-    // DOM 노드 생성, 타입 관리
-    const dom =
-        element.type === "TEXT_ELEMENT" 
-        ? document.createTextNode("")
-        : document.createElement(element.type)
-    
-    // props에서 children이 아닌 애들만
-    const isProperty = key => key !== "children"
-    // 키만 뽑아 배열로 반환
-    Object.keys(element.props)
-    .filter(isProperty)
-    .forEach(name => {
-        // 노드에 엘리먼트 속성 부여
-        dom[name] = element.props[name]
-    })
+// DOM 노드 생성
+function createDom(fiber) {
+  // 타입 관리
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(fiber.type);
 
-    // 자식 연결, 자식들 돌며 재귀적으로 수행 -> 노드 생성 후 dom에 추가
-    element.props.children.forEach(child => {
-        render(child, dom)
+  // props에서 children이 아닌 애들만
+  const isProperty = (key) => key !== "children";
+  // 키만 뽑아 배열로 반환
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      // 노드에 엘리먼트 속성 부여
+      dom[name] = fiber.props[name];
     });
 
-    // 컨테이너에 DOM 노드 추가
-    container.appendChild(dom)
+  return dom;
+}
+
+// fiber 트리의 루트에 nextUnitOfWork 함수 설정
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+        children: [element],
+    }
+  }
 }
 
 // 동시성 모드, 렌더링 도중 끼어들 수 있도록 구현
-let nextUnitOfWork = null // 첫번째 작업 단위
+let nextUnitOfWork = null; // 첫번째 작업 단위
+
 // deadline: 다시 브라우저에서 제어를 가져갈 때까지 얼마나 걸리는지 체크
 function workLoop(deadline) {
-    let shouldYield = false
-    while (nextUnitOfWork && !shouldYield) {
-        // 다음 작업 단위 반환
-        nextUnitOfWork = performUnitOfWork(
-            nextUnitOfWork
-        )
-        shouldYield = deadline.timeRemaining() < 1
-    }
-    requestIdleCallback(workLoop)
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    // 다음 작업 단위 반환
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
 }
 
 // 메인 스레드가 대기 상태일 때 브라우저가 콜백을 실행
-requestIdleCallback(workLoop)
+requestIdleCallback(workLoop);
 
-function performUnitOfWork(nextUnitOfWork) {
+// fiber 기능
+function performUnitOfWork(fiber) {
+    // DOM에 엘리먼트 추가
+    // 루트부터 작업을 시작할 떄 새로운 노드 생성 후 추가
+    if(!fiber.dom) {
+        fiber.dom = createDom(fiber);
+    }
 
+    
+    if (fiber.parent) {
+        fiber.parent.dom.appendchild(fiber.dom)
+    }
+
+    // 각 엘리먼트의 children에 대해 fiber 생성
+    const elements = fiber.props.children
+    let index = 0
+    let prevSibling = null
+
+    // index가 elements배열 길이보다 작을 때 반복
+    while (index < elements.length) {
+        const element = elements[index]
+
+        // 새로운 fiber 생성
+        const newFiber = {
+            type: element.type,
+            props: element.props,
+            parent: fiber,
+            dom: null,
+        }
+
+        // 첫번째 자식이면 
+        if (index === 0) {
+            // 자식으로 fiber 트리에 추가
+            fiber.child = newFiber
+        }else {
+            // 형제자매로 fiber 트리에 추가
+            prevSibling.sibling = newFiber
+        }
+    
+        // 이전 형제를 생성 fiber로
+        prevSibling = newFiber
+        index++
+    }
+
+    // 다음 작업 단위 선택
+    // 자식 탐색
+    if(fiber.child) {
+        return fiber.children
+    }
+
+    // 형제 자매로 이동
+    let nextFiber = fiber
+
+    while (nextFiber) {
+        // 형제자매 탐색
+        if(nextFiber.sibling) {
+            return nextFiber.sibling
+        }
+        // 부모의 형제자매로 이동
+        nextFiber = nextFiber.parent
+    }
 }
 
 const Didact = {
-    createElement,
-    render,
-}
+  createElement,
+  render,
+};
 
 // jsx 사용
 /**@jsx Didact.createElement */
 const element = (
-    <div id="foo">
-        <a>bar</a>
-        <b />
-    </div>
-)
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+);
 
-const container = document.getElementById("root")
-Didact.render(element, container)
+const container = document.getElementById("root");
+Didact.render(element, container);
