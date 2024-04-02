54d2c6fcbf7102a764e945e2ca53a1affbcd1b4c
김다인 <dbslzhs1218@naver.com>
2024-04-02 21:11:18 +0900

    feat: Step2: render 함수

--- build_own_your_react/didact.js
+++ build_own_your_react/didact.js
@@ -44,8 +44,36 @@ function createTextElement (text) {
     }
 }
 
+// ReactDOM.render 함수 구현, DOM 갱신과 삭제
+function render(element, container) {
+    // DOM 노드 생성, 타입 관리
+    const dom =
+        element.type === "TEXT_ELEMENT" 
+        ? document.createTextNode("")
+        : document.createElement(element.type)
+    
+    // props에서 children이 아닌 애들만
+    const isProperty = key => key !== "children"
+    // 키만 뽑아 배열로 반환
+    Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+        // 노드에 엘리먼트 속성 부여
+        dom[name] = element.props[name]
+    })
+
+    // 자식 연결, 자식들 돌며 재귀적으로 수행 -> 노드 생성 후 dom에 추가
+    element.props.children.forEach(child => {
+        render(child, dom)
+    });
+
+    // 컨테이너에 DOM 노드 추가
+    container.appendChild(dom)
+}
+
 const Didact = {
     createElement,
+    render,
 }
 
 // jsx 사용
@@ -58,4 +86,4 @@ const element = (
 )
 
 const container = document.getElementById("root")
-ReactDOM.render(element, container)
+Didact.render(element, container)
